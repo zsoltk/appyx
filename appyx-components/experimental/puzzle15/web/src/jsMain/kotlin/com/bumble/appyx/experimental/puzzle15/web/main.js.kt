db32e3d9d7d9772c6d365d52b6da1142f9abf554
Manel Martos <manuel.martos-roldan@team.bumble.com>
2024-03-09 19:00:20 +0100

    Use CanvasBasedWindow instead of Window as it has been removed from API

--- appyx-components/experimental/puzzle15/web/src/jsMain/kotlin/com/bumble/appyx/experimental/puzzle15/web/main.js.kt
+++ appyx-components/experimental/puzzle15/web/src/jsMain/kotlin/com/bumble/appyx/experimental/puzzle15/web/main.js.kt
@@ -9,19 +9,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.CanvasBasedWindow
 import com.bumble.appyx.components.experimental.puzzle15.ui.Puzzle15Ui
 import org.jetbrains.skiko.wasm.onWasmReady
 
+@OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     onWasmReady {
-        Window("Puzzle15") {
+        CanvasBasedWindow("Puzzle15") {
             val requester = remember { FocusRequester() }
             var size by remember { mutableStateOf(IntSize.Zero) }
             Surface(
