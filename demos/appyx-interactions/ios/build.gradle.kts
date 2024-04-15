d4d687f87b09bf940827b0f76c96be5b17e3d059
Manel Martos <manuel.martos-roldan@team.bumble.com>
2024-04-15 23:50:38 +0200

    Fix compile time issues with iOS samples

--- demos/appyx-interactions/ios/build.gradle.kts
+++ demos/appyx-interactions/ios/build.gradle.kts
@@ -1,28 +1,20 @@
 plugins {
     id("com.bumble.appyx.multiplatform")
     kotlin("multiplatform")
-    kotlin("native.cocoapods")
     id("org.jetbrains.compose")
     id("com.google.devtools.ksp")
 }
 
 kotlin {
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
-
-    cocoapods {
-        version = "1.0.0"
-        summary = "appyx-interactions ios module"
-        homepage = "https://bumble-tech.github.io/appyx/interactions/"
-        ios.deploymentTarget = "16.4"
-        podfile = project.file("../iosApp/Podfile")
-        framework {
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
             baseName = "ios"
             isStatic = true
         }
-        license = "Apache License, Version 2.0"
-        authors = "https://github.com/bumble-tech/"
     }
 
     sourceSets {
